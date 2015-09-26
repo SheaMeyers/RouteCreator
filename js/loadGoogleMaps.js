4c4d005b28199769a3374c51c5e609d480e797fb
SheaMeyers <shea.meyers@gmail.com>
2015-09-26 17:48:18 -0600

    Updated Google Maps to have a Developer API key

--- js/loadGoogleMaps.js
+++ js/loadGoogleMaps.js
@@ -1,10 +1,8 @@
 
-function initialize() {
-var mapProp = {
-  center:new google.maps.LatLng(52.1333,-106.6833),
-  zoom:11,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('GoogleMap'), {
+    center: {lat: 52.1333, lng: -106.6833},
+    zoom: 11
+  });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
